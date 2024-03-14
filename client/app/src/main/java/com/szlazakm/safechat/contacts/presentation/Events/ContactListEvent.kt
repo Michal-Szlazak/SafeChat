3eaf1b02bb11b929d00bed8797a59377d7e626eb
szlazakm <01169634@pw.edu.pl>
2024-03-14 18:30:46 +0100

    Updated states and events for screns

--- client/app/src/main/java/com/szlazakm/safechat/contacts/presentation/Events/ContactListEvent.kt
+++ client/app/src/main/java/com/szlazakm/safechat/contacts/presentation/Events/ContactListEvent.kt
@@ -4,9 +4,5 @@ import com.szlazakm.safechat.contacts.domain.Contact
 
 sealed interface ContactListEvent {
     data class OnConversationClick(val contact: Contact): ContactListEvent
-    object DismissConversation: ContactListEvent
     object OnNewConversationClick: ContactListEvent
-    object DismissNewConversation: ContactListEvent
-    data class OnFirstNameChanged(val value: String): ContactListEvent
-    data class OnLastNameChanged(val value: String): ContactListEvent
 }
--- client/app/src/main/java/com/szlazakm/safechat/contacts/presentation/States/ContactListState.kt
+++ client/app/src/main/java/com/szlazakm/safechat/contacts/presentation/States/ContactListState.kt
@@ -5,7 +5,5 @@ import com.szlazakm.safechat.contacts.domain.Contact
 data class ContactListState(
     val contacts: List<Contact> = emptyList(),
     val recentlyActiveContacts: List<Contact> = emptyList(),
-    val selectedContact: Contact? = null,
-    val isSelectedChatSheetOpen: Boolean = false,
-    val isAddChatSheetOpen: Boolean = false
+    val selectedContact: Contact? = null
 )
